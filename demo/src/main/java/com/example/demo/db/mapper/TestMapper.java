d97f9de7ca79be5fe209a3f6945eeb2b7fc4b66c
jinklee <jinklee@HLBRD10-NE104PL.LGE.NET>
2019-10-18 17:50:33 +0900

    sample test

---
+++ demo/src/main/java/com/example/demo/db/mapper/TestMapper.java
@@ -0,0 +1,11 @@
+package com.example.demo.db.mapper;
+
+import java.util.List;
+
+import com.example.demo.db.dto.Test;
+
+public interface TestMapper {
+	 
+    public List<Test> getAll() throws Exception;
+    
+}
